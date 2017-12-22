f9e68a20588611a04b0fa2b560d5cf82adfe4607
heifade <heifade@126.com>
2017-12-22 13:45:57 +0800

    tools

--- tools/formatDocs.js
+++ tools/formatDocs.js
@@ -1,61 +1,67 @@
-
-// 替换 doc 目录下面所有"_" 打头的文件
-let fs = require("fs");
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const fs_i_1 = require("fs-i");
 let renameFileList = [];
-
-// 遍历目录，查找所有文件
-function eachFile(path, callback) {
-  var each = function(path) {
-    if (fs.existsSync(path)) {
-      let files = fs.readdirSync(path);
-      files.forEach((file, index) => {
-        let fileFullName = `${path}/${file}`;
-        if (fs.statSync(fileFullName).isDirectory()) {
-          each(fileFullName);
-        } else {
-          callback({ path, file });
-        }
-      });
-    }
-  };
-
-  each(path);
+function rename(fileName) {
+    return fileName
+        .replace(/\/_/, "/")
+        .replace(/_\//g, "/")
+        .replace(/\/_/g, "/")
+        .replace(/_\./g, ".")
+        .replace(/_/g, ".");
 }
-
-// 遍历目录，重命名所有带"_"的文件
 function renameFile(path) {
-  eachFile(path, file => {
-    var fromName = file.file;
-    var toName = file.file.replace(/_/g, "A");
-    fs.renameSync(file.path + "/" + fromName, file.path + "/" + toName);
-
-    if (fromName != toName) {
-      renameFileList.push({
-        fromName,
-        toName
-      });
-    }
-  });
+    return __awaiter(this, void 0, void 0, function* () {
+        let fileList = yield fs_i_1.getAllFiles(path);
+        for (let fileName of fileList) {
+            var toName = rename(fileName);
+            if (fileName != toName) {
+                fs_1.renameSync(fileName, toName);
+                renameFileList.push({
+                    sourceName: fileName.substr(fileName.lastIndexOf("/") + 1),
+                    targetName: toName.substr(toName.lastIndexOf("/") + 1)
+                });
+            }
+        }
+    });
 }
-
-// 在文件中更新文件引用
 function replaceFile(path) {
-  eachFile(path, file => {
-    let fileName = file.path + "/" + file.file;
-    if (
-      fileName.endsWith(".html") ||
-      fileName.endsWith(".js") ||
-      fileName.endsWith(".htm")
-    ) {
-      var content = fs.readFileSync(fileName, "utf-8");
-      renameFileList.map(h => {
-        content = content.replace(new RegExp(h.fromName, 'g'), h.toName);
-      });
-      fs.writeFileSync(fileName, content);
-    }
-  });
+    return __awaiter(this, void 0, void 0, function* () {
+        let fileList = yield fs_i_1.getAllFiles(path);
+        for (let fileName of fileList) {
+            if (fileName.endsWith(".html") ||
+                fileName.endsWith(".js") ||
+                fileName.endsWith(".htm")) {
+                var content = fs_1.readFileSync(fileName, "utf-8");
+                renameFileList.map(h => {
+                    content = content.replace(new RegExp(h.sourceName, "g"), h.targetName);
+                });
+                fs_1.writeFileSync(fileName, content);
+            }
+        }
+    });
+}
+function format(docPath) {
+    (function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield renameFile(docPath);
+            yield replaceFile(docPath);
+        });
+    })()
+        .then()
+        .catch(err => {
+        console.log(err);
+    });
 }
 
-renameFile("./docs");
-replaceFile("./docs");
+
+format('./docs');
